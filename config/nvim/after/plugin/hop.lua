08a98504bb3c47922fa121de9229ad2d06509546
Moaid Hathot <moaidhathot@microsoft.com>
2023-05-15 16:56:07 +0300

    Fixing the 'hop line' shortcut for 'ls'

--- config/nvim/after/plugin/hop.lua
+++ config/nvim/after/plugin/hop.lua
@@ -5,6 +5,7 @@ hop.setup { keys = 'etovxqpdygfblzhckisuran', case_insensitive = false }
 
 vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
 vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+vim.keymap.set('', 'ls', ":HopLine<CR>", { desc = 'Hop Line', silent = true })
 -- char
 vim.keymap.set('', 'f', function()
 	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
@@ -18,5 +19,3 @@ end, { remap = true })
 vim.keymap.set('', 'T', function()
 	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
 end, { remap = true })
-
-vim.keymap.set('', 'S', ":HopLine<CR>", { desc = 'Hop Line', silent = true })
