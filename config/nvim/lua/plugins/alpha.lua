0efddd7350c68d2ede4dd28ef71e375d2554ea89
Moaid Hathot <moaid.hathot@outlook.com>
2024-02-27 15:24:40 +0200

    remove a comment

--- config/nvim/lua/plugins/alpha.lua
+++ config/nvim/lua/plugins/alpha.lua
@@ -30,7 +30,6 @@ return {
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	config = function()
 		local alpha = require('alpha')
-		-- local dashboard = require('alpha.themes.startify')
 		local dashboard = require('alpha.themes.dashboard')
 
 		dashboard.section.buttons.val = {
