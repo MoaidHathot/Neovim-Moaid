4cb50d7973225f2f378ebf7bf46cfbbf76e6168b
Moaid Hathot <moaid.hathot@outlook.com>
2024-02-27 15:56:36 +0200

    removing `fixjson` and fixing "Close all buffers but this"

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -60,7 +60,7 @@ return {
 				sources = {
 					null_ls.builtins.formatting.stylua,
 					null_ls.builtins.formatting.csharpier,
-					null_ls.builtins.formatting.fixjson,
+					-- null_ls.builtins.formatting.jsonls,
 					null_ls.builtins.formatting.yamlfmt,
 					null_ls.builtins.formatting.black,
 					null_ls.builtins.formatting.isort,
