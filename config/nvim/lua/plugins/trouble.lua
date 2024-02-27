f1dea3c4735d56b1b744cad00e3c26f7b07bb218
Moaid Hathot <moaid.hathot@outlook.com>
2024-02-27 18:38:01 +0200

    lsp keymapping

--- config/nvim/lua/plugins/trouble.lua
+++ config/nvim/lua/plugins/trouble.lua
@@ -5,14 +5,4 @@ return {
 	opts = {
 		auto_preview = true
 	},
-	config = function()
-		vim.keymap.set('n', '<leader>lq', ":TroubleToggle document_diagnostics<CR>",
-			{ desc = "Toggle Document Diagnostics" })
-		vim.keymap.set('n', '<leader>ld', ":TroubleToggle document_diagnostics<CR>",
-			{ desc = "Toggle Document Diagnostics" })
-		vim.keymap.set('n', '<leader>ll', ':TroubleToggle loclist<CR>', { desc = "Toggle Locallist" })
-		vim.keymap.set('n', '<leader>lw', ':TroubleToggle workspace_diagnostics<CR>',
-			{ desc = "Toggle Workspace Diagnostics" })
-		vim.keymap.set('n', '<leader>lr', ':TroubleToggle lsp_references<CR>', { desc = "Toggle LSP References" })
-	end
 }
--- old/config/nvim/after/plugin/trouble.lua
+++ old/config/nvim/after/plugin/trouble.lua
@@ -6,7 +6,11 @@ trouble.setup {
 
 vim.keymap.set('n', '<leader>lq', ":TroubleToggle document_diagnostics<CR>", { desc = "Toggle Document Diagnostics" })
 vim.keymap.set('n', '<leader>ld', ":TroubleToggle document_diagnostics<CR>", { desc = "Toggle Document Diagnostics" })
+vim.keymap.set('n', '<leader>lD', ":TroubleToggle lsp_definitions<CR>", { desc = "Toggle LSP Definitions" })
 -- vim.keymap.set('n', '<leader>la', ':TroubleToggle quickfix<CR>')
 vim.keymap.set('n', '<leader>ll', ':TroubleToggle loclist<CR>', { desc = "Toggle Locallist" })
 vim.keymap.set('n', '<leader>lw', ':TroubleToggle workspace_diagnostics<CR>', { desc = "Toggle Workspace Diagnostics" })
-vim.keymap.set('n', '<leader>lr', ':TroubleToggle lsp_references<CR>', { desc = "Toggle LSP References" })
+vim.keymap.set('n', '<leader>lR', ':TroubleToggle lsp_references<CR>', { desc = "Toggle LSP References" })
+vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, { desc = "toggle lsp references" })
+vim.keymap.set('n', '<f2>', vim.lsp.buf.rename, { desc = "toggle lsp references" })
+vim.keymap.set('n', '<leader>lI', ':TroubleToggle lsp_implementations<CR>', { desc = "Toggle LSP References" })
