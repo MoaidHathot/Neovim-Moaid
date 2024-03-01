ab903de4a53f34c235d8ceeea036e7c24b871efd
Moaid Hathot <moaid.hathot@outlook.com>
2024-03-02 00:33:10 +0200

    small change to flash

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -14,32 +14,29 @@ return {
 			keys = 'etovxqpdygfblzhckisuran'
 		}
 		-- config = function()
-			-- you can configure Hop the way you like here; see :h hop-config
-			-- require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+		-- you can configure Hop the way you like here; see :h hop-config
+		-- require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
 
-			-- vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
-			-- vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
-			-- vim.keymap.set('', 'ls', ":HopLine<CR>", { desc = 'Hop Line', silent = true })
-			-- end
+		-- vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+		-- vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+		-- vim.keymap.set('', 'ls', ":HopLine<CR>", { desc = 'Hop Line', silent = true })
+		-- end
 	},
 	{
-"folke/flash.nvim",
-  -- event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {
-	  modes = {
-		  char = {
-			  enabled = false,
-		  }
-	  }
-  },
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+		"folke/flash.nvim",
+		-- event = "VeryLazy",
+		---@type Flash.Config
+		opts = {
+			modes = {
+				char = {
+					enabled = false,
+				}
+			}
+		},
+		-- stylua: ignore
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+			-- { "S", mode = { "n", "x", "o" }, function() require("flash").Treesitter() end, desc = "Flash Treesitter" },
+		},
 	}
 }
