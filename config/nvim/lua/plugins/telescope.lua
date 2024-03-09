c7fb807255f3ba45ac7e7e05b5585bbb5bc0010b
Moaid Hathot <moaidhathot@microsoft.com>
2024-03-09 11:13:32 -0800

    adding fzf-native

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -56,6 +56,7 @@ return {
 	},
 	{
 		'nvim-telescope/telescope-ui-select.nvim',
+		'nvim-telescope/telescope-fzf-native.nvim',
 		-- event = "VeryLazy"
 		cmd = "Telescope",
 		config = function()
@@ -76,10 +77,21 @@ return {
 							--hijack_newtrw = true
 						}
 					},
+					fzf = {
+						fuzzy = true, -- false will only do exact matching
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+					}
 				}
 			})
 
 			telescope.load_extension('ui-select')
+			telescope.load_extension('fzf')
 		end
+	},
+	{
+		'nvim-telescope/telescope-fzf-native.nvim',
+		build = 'make'
 	}
 }
