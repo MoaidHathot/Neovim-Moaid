132e3fde02f7224ce0bb3a88daa64692f16b001e
Moaid Hathot <moaidhathot@microsoft.com>
2024-03-13 15:05:37 -0700

    remove empty line

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -67,7 +67,6 @@ return {
 				capabilities = capabilities
 			})
 
-
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 			-- vim.keymap.set({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, {})
