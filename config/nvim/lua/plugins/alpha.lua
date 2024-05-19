bcec973aafad1f3cfec7551f444bace0ba39602d
= <=>
2024-05-19 11:57:22 -0700

    auto show code if session exists instead of alpha

--- config/nvim/lua/plugins/alpha.lua
+++ config/nvim/lua/plugins/alpha.lua
@@ -25,6 +25,7 @@ local name = {
 
 return {
 	'goolord/alpha-nvim',
+	-- enabled = false,
 	-- event = "VeryLazy",
 	-- lazy = true,
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -33,20 +34,18 @@ return {
 		local dashboard = require('alpha.themes.dashboard')
 
 		dashboard.section.buttons.val = {
-			dashboard.button('r', " Restore session", "<cmd>SessionRestore<CR>"),
-			dashboard.button('s', "ﴬ Find session", '<cmd>lua require("auto-session.session-lens").search_session() <CR>'),
-			dashboard.button('f', " Find file", "<cmd>Telescope find_files<CR>"),
-			dashboard.button('t', " Find text", "<cmd>Telescope live_grep<CR>"),
-			-- dashboard.button('p', ""),
-			-- dashboard.button('p', ""),
-			dashboard.button('n', "  New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button('o', " Recently opened files", "<cmd>Telescope oldfiles<CR>"),
+			dashboard.button('r', "󰦛 Restore session", "<cmd>SessionRestore<CR>"),
+			dashboard.button('s', "󱎰 Find session", '<cmd>lua require("auto-session.session-lens").search_session() <CR>'),
+			dashboard.button('f', "󰱼  Find file", "<cmd>Telescope find_files<CR>"),
+			dashboard.button('t', "󱎸 Find text", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button('n', "󰈔 New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button('o', "󱑒 Recently opened files", "<cmd>Telescope oldfiles<CR>"),
 			dashboard.button('u', " Update Plugins", "<cmd>Lazy sync<CR>"),
 			dashboard.button('q', " Quit", ":qa<CR>")
 		}
 
 		dashboard.section.header.val = name
-		dashboard.section.footer.val = " Moaid Hathot"
+		dashboard.section.footer.val = "Moaid Hathot"
 
 		alpha.setup(dashboard.opts)
 
--- config/nvim/lua/plugins/session.lua
+++ config/nvim/lua/plugins/session.lua
@@ -1,20 +1,18 @@
 return {
 	{
 		'rmagatti/auto-session',
-		event = "VeryLazy",
-		keys = {
-			{ '<leader>ss', function() require("auto-session.session-lens").search_session() end, { desc = "Search Session" } },
-		},
-		cmd = "SessionRestore",
+		-- event = "VeryLazy",
+		-- keys = {
+		-- 	{ '<leader>ss', function() require("auto-session.session-lens").search_session() end, { desc = "Search Session" } },
+		-- },
+		-- cmd = "SessionRestore",
 		config = function()
 			require('auto-session').setup({
 				log_level = 'error',
-				auto_restore_enabled = true
+				auto_restore_enabled = true,
 			})
-
-			-- require("auto-session.session-lens").search_session()
-			-- vim.keymap.set('n', '<leader>ss', require("auto-session.session-lens").search_session,
-			-- 	{ desc = "Search Session" })
+			vim.keymap.set('n', '<leader>ss', function() require("auto-session.session-lens").search_session() end,
+				{ desc = "Search Session" })
 		end
 	}
 }
