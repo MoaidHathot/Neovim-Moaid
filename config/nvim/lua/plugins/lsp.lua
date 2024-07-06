19919f54202b7f368f533129734fedef00aa0a70
Moaid Hathot <Moaid.Hathot@outlook.com>
2024-07-06 01:07:59 -0700

    Update configuration

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -81,6 +81,11 @@ return {
 				capabilities = capabilities
 			})
 
+			lspconfig.codeql.setup({
+				capabilities = capabilities
+			})
+
+
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 			-- vim.keymap.set({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, {})
