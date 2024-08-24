15afa199975e42e117ac1a180f9be422ed04ea60
Moaid Hathot <Moaid.Hathot@outlook.com>
2024-08-24 16:44:23 -0700

    Updating GlazeWM configuration and PS' profile

--- config/powershell/profile.ps1
+++ config/powershell/profile.ps1
@@ -23,18 +23,41 @@ function Get-LocalAppData
 
 function Start-Glaze
 {
-	$repoPath = "$env:Moaid_Config_Path/config/glazewm/glazewm_config.yaml"
-	write-host $repoPath
-	glazewm start --config="$repoPath"
+	$glazeConfigPath = "$env:Moaid_Config_Path/config/glazewm/glazewm_config.yaml"
+	write-host $glazeConfigPath
+	glazewm start --config="$glazeConfigPath"
 }
 
 function Start-Zebar
 {
-	# & "$env:USERPROFILE/.glzr\zebar\start_zebar.ps1"
 	$monitors = zebar monitors
 	foreach ($monitor in $monitors) {
 	 Start-Process -WindowStyle Hidden -FilePath "zebar" -ArgumentList "open bar --args $monitor"
 	};
+}
+
+function Stop-Zebar
+{
+	Get-Process | Where-Object { $_.ProcessName -eq "zebar" } | Foreach-Object { Stop-Process -id $_.id -Force }
+}
+
+function Restart-Zebar($overrideConfigs = $true)
+{
+	if ($overrideConfigs)
+	{
+		Update-Zebar-Config
+	}
+
+	Stop-Zebar
+	Start-Zebar
+}
+
+function Update-Zebar-Config
+{
+	$zebarConfigPath = "$env:Moaid_Config_Path/config/glazewm/zebar_config.yaml"
+	$zebarDefaultConfigPath = "$env:USERPROFILE/.glzr/zebar/config.yaml"
+
+	Copy-Item -Path $zebarConfigPath -Destination $zebarDefaultConfigPath -Force
 }
 
 function Update-Path
