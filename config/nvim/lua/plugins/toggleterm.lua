7d11396dc0c45358aa00966b87475552952b2017
Moaid Hathot <Moaid.Hathot@outlook.com>
2025-01-23 03:33:53 -0800

    improve, mainly refactor toggleterm

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -1,38 +1,55 @@
 return {
 	'akinsho/toggleterm.nvim',
 	-- event = "VeryLazy",
-	cmd = "ToggleTerm",
+	-- cmd = "ToggleTerm",
 	keys = {
 
 		{ '<C-\\>',     '<cmd>:1ToggleTerm direction=float<CR>',              mode = { 'n', 'i', 't' } },
-		{ '<M-F1>',      '<cmd>:2ToggleTerm direction=horizontal size=20<CR>', mode = { 'n', 't' } },
-		{ '<M-F2>',      '<cmd>:3ToggleTerm direction=vertical size=100<CR>',  mode = { 'n', 't' } },
-		{ '<M-F3>',      '<cmd>:4ToggleTerm direction=float<CR>',              mode = { 'n', 't' } },
+		{ '<M-F1>',     '<cmd>:2ToggleTerm direction=horizontal size=20<CR>', mode = { 'n', 't' } },
+		{ '<M-F2>',     '<cmd>:3ToggleTerm direction=vertical size=100<CR>',  mode = { 'n', 't' } },
+		{ '<M-F3>',     '<cmd>:4ToggleTerm direction=float<CR>',              mode = { 'n', 't' } },
 		{ '<leader>gl', "",                                                   mode = { 'n', 't' } },
 		{ '<leader>gl', function() end,                                       mode = { 'n', 't' } },
 	},
 	version = "*",
+	opts = {
+		start_in_insert = true,
+		terminal_mappings = true,
+		-- direction = 'float',
+		-- shell = "pwsh.exe -NoLogo -NoProfile",
+		shell = "pwsh.exe -NoLogo",
+		auto_scroll = true,
+		-- persist_mode = true,
+		persist_size = true,
+		close_on_exit = true,
+	},
 	config = function()
-		require('toggleterm').setup({
+		require("toggleterm").setup({ })
+		-- require('toggleterm').setup({
+		--
+		-- 	start_in_insert = true,
+		-- 	terminal_mappings = true,
+		-- 	-- direction = 'float',
+		-- 	-- shell = "pwsh.exe -NoLogo -NoProfile",
+		-- 	shell = "pwsh.exe -NoLogo",
+		-- 	auto_scroll = true,
+		-- 	-- persist_mode = true,
+		-- 	persist_size = true,
+		-- 	close_on_exit = true,
+		-- })
 
-			start_in_insert = true,
-			terminal_mappings = true,
-			-- direction = 'float',
-			-- shell = "pwsh.exe -NoLogo -NoProfile",
-			shell = "pwsh.exe -NoLogo",
-			auto_scroll = true,
-			-- persist_mode = true,
-			persist_size = true,
-			close_on_exit = true,
-		})
+		-- function _lazygit_toggle()
+			-- local Terminal = require('toggleterm.terminal').Terminal
+			-- local lazygit = Terminal:new({ cmd = 'lazygit', hidden = true, direction = 'float' })
+			-- lazygit:toggle()
+		-- end
 
-		local Terminal = require('toggleterm.terminal').Terminal
-		local lazygit = Terminal:new({ cmd = 'lazygit', hidden = true, direction = 'float' })
-
-		function _lazygit_toggle()
+		-- vim.keymap.set({ 'n', 't' }, '<leader>gl', function() _lazygit_toggle() end)
+		vim.keymap.set({ 'n', 't' }, '<leader>gl', function() 
+			local Terminal = require('toggleterm.terminal').Terminal
+			local lazygit = Terminal:new({ cmd = 'lazygit', hidden = true, direction = 'float' })
 			lazygit:toggle()
-		end
 
-		vim.keymap.set({ 'n', 't' }, '<leader>gl', function() _lazygit_toggle() end)
+		end)
 	end
 }
