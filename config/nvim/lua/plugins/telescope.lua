80f03d2c4515d3298dca761bc5767b31ccb12dbe
Moaid Hathot <Moaid.Hathot@outlook.com>
2025-01-29 02:03:41 -0800

    Smart "FD" - WIP

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,179 @@
+local extract_args = function(prompt, opts)
+	if not prompt or prompt == "" then
+		return nil
+	end
+
+	local pieces = vim.split(prompt, opts.delimeter)
+
+	local prompt_value = prompt
+
+	if pieces[1] then
+		prompt_value = pieces[1]
+	end
+
+	local type = nil
+	local more_args_pieces = nil
+	local more_more_pieces = nil
+
+	local command_args = {}
+
+	if pieces[2] then
+		if pieces[2]:find("%*") then
+			type = pieces[2]
+		elseif pieces[2]:find("%-") then
+			more_args_pieces = pieces[2]
+		end
+
+		if pieces[3] then
+			if pieces[3]:find("%*") then
+				type = pieces[3]
+			elseif pieces[3]:find("%-") then
+				more_more_pieces = pieces[3]
+			end
+		end
+	end
+
+	if more_args_pieces then
+		for match in more_args_pieces:gmatch("%-%-%S+") do
+			table.insert(command_args, match)
+		end
+	end
+
+	if more_more_pieces then
+		for match in more_more_pieces:gmatch("%-%-%S+") do
+			table.insert(command_args, match)
+		end
+	end
+
+	return {
+		prompt = prompt_value,
+		type = type,
+		command_args = command_args
+	};
+
+end
+
+local smart_grep = function(opts)
+	opts.cwd = opts.cwd or vim.uv.cwd()
+	opts.delimeter = opts.delimeter or "  "
+	opts.title = opts.title or "Smart Grep"
+
+	local pickers = require('telescope.pickers')
+	local finders = require('telescope.finders')
+	local make_entry = require('telescope.make_entry')
+	local config = require('telescope.config').values
+
+	local finder = finders.new_async_job({
+		command_generator = function(prompt)
+			if not prompt or prompt == "" then
+				return nil
+			end
+
+			local args = { "rg" }
+
+			local extracted = extract_args(prompt, opts)
+			print("Extracted", vim.inspect(extracted))
+
+			if not extracted then
+				return nil
+			end
+
+			if extracted.prompt then
+				table.insert(args, "-e")
+				table.insert(args, extracted.prompt)
+			end
+
+			if extracted.type then
+				table.insert(args, "-g")
+				table.insert(args, extracted.type)
+			end
+
+			local command_args = extracted.command_args or {}
+			table.insert(command_args, { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case", "--follow" })
+
+			local final_args = vim.tbl_flatten {
+				args,
+				command_args
+			}
+
+			return final_args
+		end,
+
+		entry_maker = make_entry.gen_from_vimgrep(opts),
+		cwd = opts.cwd,
+	})
+
+	pickers.new(opts, {
+		debounce = 100,
+		prompt_title = opts.title,
+		finder = finder,
+		previewer = config.grep_previewer(opts),
+		sorter = require('telescope.sorters').empty(),
+	}):find()
+end
+
+local smart_fd = function(opts)
+	opts.cwd = opts.cwd or vim.uv.cwd()
+	opts.delimeter = opts.delimeter or "  "
+	opts.title = opts.title or "Smart File Search"
+
+	local pickers = require('telescope.pickers')
+	local finders = require('telescope.finders')
+	local make_entry = require('telescope.make_entry')
+	local config = require('telescope.config').values
+
+	local finder = finders.new_async_job({
+		command_generator = function(prompt)
+			if not prompt or prompt == "" then
+				return nil
+			end
+
+			local args = { "fd" }
+
+			local extracted = extract_args(prompt, opts)
+			print("Extracted", vim.inspect(extracted))
+
+			if not extracted then
+				return nil
+			end
+
+			if extracted.prompt then
+				-- table.insert(args, "-e")
+				table.insert(args, extracted.prompt)
+			end
+
+			if extracted.type then
+				table.insert(args, "-e")
+				table.insert(args, extracted.type)
+			end
+
+			local command_args = extracted.command_args or {}
+			table.insert(command_args, { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case", "--follow" })
+
+			local final_args = vim.tbl_flatten {
+				args,
+				command_args
+			}
+
+			print(vim.inspect(final_args))
+
+			return final_args
+		end,
+
+		entry_maker = make_entry.gen_from_vimgrep(opts),
+		cwd = opts.cwd,
+	})
+
+	pickers.new(opts, {
+		debounce = 100,
+		prompt_title = opts.title,
+		finder = finder,
+		previewer = config.grep_previewer(opts),
+		sorter = require('telescope.sorters').empty(),
+	}):find()
+end
+
+
 return {
 	{
 		'nvim-telescope/telescope.nvim',
@@ -39,14 +215,14 @@ return {
 			{ '<leader>sF', "<cmd>Telescope find_files hidden=true no_ignore=true<CR>", { desc = 'Find All Files' } },
 			{ '<leader>sf', function() require('telescope.builtin').find_files() end,   { desc = 'Find Files' } },
 			-- vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = 'Find Files' })
-			{ '<leader>sg', function() require('telescope.builtin').live_grep() end,    { desc = 'Find Grep' } },
-			{ '<leader>sG',
-				function()
-					require('telescope.builtin').live_grep { additional_args = function(args)
-						return vim.list_extend(args,
-							{ '--hidden', '--no-ignore' })
-					end }
-				end, { desc = 'Find Grep Everything' } },
+			-- { '<leader>sg', function() require('telescope.builtin').live_grep() end,    { desc = 'Find Grep' } },
+			-- { '<leader>sG',
+			-- 	function()
+			-- 		require('telescope.builtin').live_grep { additional_args = function(args)
+			-- 			return vim.list_extend(args,
+			-- 				{ '--hidden', '--no-ignore' })
+			-- 		end }
+			-- 	end, { desc = 'Find Grep Everything' } },
 			{ '<leader>sb', function() require('telescope.builtin').buffers() end,                              { desc = 'Find Buffers' } },
 			{ '<leader>sh', function() require('telescope.builtin').help_tags() end,                            { desc = 'Find Help' } },
 			{ '<leader>sc', function() require('telescope.builtin').current_buffer_fuzzy_find() end,            { desc = 'Find in current buffer' } },
@@ -76,98 +252,8 @@ return {
 
 			{ '<leader>st', function() require('telescope.builtin').builtin() end,                              { desc = 'Find Telescope Pickers' } },
 			{ '<leader>sT', function() require('telescope.builtin').builtin() end,                              { desc = 'Find Telescope cached Pickers' } },
-			{ '<leader>st', function()
-				local opts = {}
-				opts.cwd = opts.cwd or vim.uv.cwd()
-				opts.delimeter = opts.delimeter or "  "
-				opts.hidden = opts.hidden or true
-				opts.ignored = opts.ignored or false
-
-				local pickers = require('telescope.pickers')
-				local finders = require('telescope.finders')
-				local make_entry = require('telescope.make_entry')
-				local config = require('telescope.config').values
-
-				local finder = finders.new_async_job({
-					command_generator = function(prompt)
-						if not prompt or prompt == "" then
-							return nil
-						end
-
-						local pieces = vim.split(prompt, opts.delimeter)
-						local args = { "rg" }
-
-						if pieces[1] then
-							table.insert(args, "-e")
-							table.insert(args, pieces[1])
-						end
-
-						local type = nil
-						local more_args_pieces = nil
-						local more_more_pieces = nil
-
-						local command_args = {}
-
-						-- print("Pieces", vim.inspect(pieces))
-
-						if pieces[2] then
-							if pieces[2]:find("%*") then
-								type = pieces[2]
-							elseif pieces[2]:find("%-") then
-								more_args_pieces = pieces[2]
-							end
-
-							if pieces[3] then
-								if pieces[3]:find("%*") then
-									type = pieces[3]
-								elseif pieces[3]:find("%-") then
-									more_more_pieces = pieces[3]
-								end
-							end
-						end
-
-						if type then
-							table.insert(args, "-g")
-							table.insert(args, type)
-						end
-
-						if more_args_pieces then
-							for match in more_args_pieces:gmatch("%-%-%S+") do
-								table.insert(command_args, match)
-							end
-						end
-
-						if more_more_pieces then
-							for match in more_more_pieces:gmatch("%-%-%S+") do
-								table.insert(command_args, match)
-							end
-						end
-
-						table.insert(command_args, { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case", "--follow" })
-
-						local final_args = vim.tbl_flatten {
-							args,
-							-- { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case", "--follow" },
-							command_args
-						}
-
-						-- print(vim.inspect(final_args))
-
-						return final_args
-					end,
-
-					entry_maker = make_entry.gen_from_vimgrep(opts),
-					cwd = opts.cwd,
-				})
-
-				pickers.new(opts, {
-					debounce = 100,
-					prompt_title = "Grep with Filter",
-					finder = finder,
-					previewer = config.grep_previewer(opts),
-					sorter = require('telescope.sorters').empty(),
-				}):find()
-			end, { desc = 'Find Grep with Filters' } }
+			{ '<leader>sg', function() smart_grep({}) end, { desc = "Find Grep with filters" }},
+			{ '<leader>sG', function() smart_fd({}) end, { desc = "Find Grep with filters" }},
 		},
 	},
 	{
