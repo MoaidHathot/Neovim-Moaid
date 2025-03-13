e215d9c4a370b75ad56392f087897e7b26d70ff9
Moaid Hathot <Moaid.Hathot@outlook.com>
2025-03-12 23:05:39 -0700

    adding the disablement of netrw

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,7 +1,7 @@
 vim.loader.enable()
 
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
