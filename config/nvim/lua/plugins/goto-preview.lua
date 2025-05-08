f820e1fd213b573bffec2a4f403007038e26a335
Moaid Hathot <Moaid.Hathot@outlook.com>
2025-05-08 03:29:02 -0700

    remove matchup and minor changes to goto-preview

--- config/nvim/lua/plugins/goto-preview.lua
+++ config/nvim/lua/plugins/goto-preview.lua
@@ -5,8 +5,9 @@ return {
 
 		{ '<leader>pd', function() require("goto-preview").goto_preview_definition() end,      { desc = "Preview Definition", silent = true } },
 		{ '<leader>pt', function() require("goto-preview").goto_preview_type_definition() end, { desc = "Preview Type Definition", silent = true } },
-		{ '<leader>pi', function() require("goto-preview").goto_preview_type_definition() end, { desc = "Preview Implementation", silent = true } },
+		{ '<leader>pi', function() require("goto-preview").goto_preview_type_implementation() end, { desc = "Preview Implementation", silent = true } },
 		{ '<leader>pr', function() require("goto-preview").goto_preview_references() end,      { desc = "Preview References", silent = true } },
+		{ '<leader>ps', function() require("goto-preview").goto_preview_declaration() end,      { desc = "Preview References", silent = true } },
 		{ '<leader>pc', function() require("goto-preview").close_all_win() end,                { desc = "Close Previews", silent = true } },
 	},
 	config = function()
