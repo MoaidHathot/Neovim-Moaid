8c07509aa2bb42aa219bf0414a815a670e4d1ba9
Moaid Hathot <Moaid.Hathot@outlook.com>
2025-05-08 03:11:17 -0700

    try lsp_signature_help but disabled

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -115,5 +115,20 @@ return {
 				})
 			})
 		end
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		enabled = false,
+		config = function()
+			require("lsp_signature").setup({
+				bind = true,
+				handler_opts = {
+					border = "rounded"
+				},
+				hint_enable = true,
+				floating_window = true,
+			})
+		end
 	}
 }
